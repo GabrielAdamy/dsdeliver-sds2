7c8051e9190fae397434951f2a82d6520f02ae37
Gabriel Adamy <adamy.g@hotmail.com>
2021-01-09 14:42:42 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -12,3 +12,12 @@ address: string;
 latitude: number;
 longitude: number;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData;
+
